90cb181ca8cd8ea095942f2fa2a9e6e29bf02885
armanleftarm <19782895+armanleftarm@users.noreply.github.com>
2017-09-04 21:31:32 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,10 +16,11 @@ command_exists() {
   command -v "$1" >/dev/null 2>&1 ;
 }
 
-PYTHON_BINARY=python
-if command_exists python2; then
-  PYTHON_BINARY=python2
-fi
+PYTHON_BINARY=python3
+
+# if command_exists python2; then
+#  PYTHON_BINARY=python
+# fi
 
 $PYTHON_BINARY "$build_file" "$@"
 
